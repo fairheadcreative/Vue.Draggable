2f0610e32879de861166989497ed888eaea37d3c
Fedor Sherbakov <mail@eqape.com>
2020-07-01 03:01:15 +0300

    Expose all scope

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,17 @@
 const config = {
   publicPath: "./",
-  configureWebpack: {
-    output: {
-      libraryExport: "default"
-    }
-  }
+  configureWebpack: {}
 };
 
-if (process.env.NODE_ENV === "production") {
-  config.configureWebpack.externals = {
-    sortablejs: {
-      commonjs: "sortablejs",
-      commonjs2: "sortablejs",
-      amd: "sortablejs",
-      root: "Sortable"
-    }
-  };
-}
+// if (process.env.NODE_ENV === "production") {
+//   config.configureWebpack.externals = {
+//     sortablejs: {
+//       commonjs: "sortablejs",
+//       commonjs2: "sortablejs",
+//       amd: "sortablejs",
+//       root: "Sortable"
+//     }
+//   };
+// }
 
 module.exports = config;
